a280c2eeeb427f75ad97abe7d0e03babace5d61c
Felipe <felipeoliveiranascimento8@gmail.com>
2023-02-22 00:31:15 -0300

    Adcionada a class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,8 +9,20 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("O problema dos 3 corpos", 365);
+        System.out.println(livro1);
         /* int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
     }
 }
